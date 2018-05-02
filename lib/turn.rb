a7af72de7933d8ac8f5193e1ff769d51c11cf218
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 07:33:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,8 +10,8 @@ def turn(board)
   puts "Please enter 1-9:"
   
   next_move = gets.strip
-    index = input_to_index(next_move)
-  
+  index = input_to_index(next_move)
+  if 
   index = -1
   until valid_move?(board, index)
     
